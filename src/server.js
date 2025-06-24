2c80fa1372774f5a7cd14cc7db5b9b472c9734ec
DaniilT048 <tis.daniil98@gmail.com>
2025-06-24 15:09:14 +0300

    maked articles list and article page

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import {getArticleById, getArticles} from "../services/articlesServices.js";
 
 const PORT = 4000;
 const app = express();
@@ -11,6 +12,7 @@ const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors({
     origin: true,
@@ -21,6 +23,17 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/articles', (req, res) => {
+    const articles = getArticles();
+    res.render('articles', { articles });
+})
+
+app.get('/articles/:id', (req, res) => {
+    const article = getArticleById(req.params.id);
+    if (!article) return res.status(404).send('Article is not found');
+    res.render('article', { article });
 });
 
 
