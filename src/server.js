deb703d7d88b40245884760ad8ca6e75037b324e
DaniilT048 <tis.daniil98@gmail.com>
2025-07-02 15:27:40 +0300

    maked theme with cookie

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import {getArticleById, getArticles} from "../services/articlesServices.js";
 import favicon from 'serve-favicon';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 const PORT = 4000;
 const app = express();
@@ -15,30 +17,49 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(favicon(path.join(__dirname, '../public/favicon.ico')));
-
+app.use(cookieParser());
 app.use(cors({
     origin: true,
+}));
+
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
 }));
 
 app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const theme = req.cookies.theme || 'light';
+    res.render('index', { theme });
 });
 
 app.get('/articles', (req, res) => {
     const articles = getArticles();
-    res.render('articles', { articles });
+    const theme = req.cookies.theme || 'light';
+    res.render('articles', { articles, theme });
 })
 
 app.get('/articles/:id', (req, res) => {
     const article = getArticleById(req.params.id);
+    const theme = req.cookies.theme || 'light';
     if (!article) return res.status(404).send('Article is not found');
-    res.render('article', { article });
+    res.render('article', {article, theme});
 });
 
+app.get('/set-theme/:theme', (req, res) => {
+    const { theme } = req.params;
 
+    if (['light', 'dark'].includes(theme)) {
+        res.cookie('theme', theme, { maxAge: 7 * 24 * 60 * 60 * 1000 }); // 7 дней
+        req.session.theme = theme;
+    }
+
+    res.redirect(req.get('Referer'));
+});
 
 
 app.listen(PORT, () => {
